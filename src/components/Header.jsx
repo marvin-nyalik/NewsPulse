ee80c9ea37a0ac98b1d1bece128550b22a110440
MARVIN <o.silverygold@gmail.com>
2023-08-08 00:06:41 +0300

    Update header and details

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import css from '../assets/css/home.module.css';
+
+const Header = () => {
+  return(
+    <div className={css.toolbar}>
+    <div className={css.headline}>
+      <div className={css.logoDiv}>
+        <i className='bx bx-news'></i>
+        <p>NewsPulse</p>
+      </div>
+      <div>
+        <i className='bx bxs-user-circle'></i>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default Header;
