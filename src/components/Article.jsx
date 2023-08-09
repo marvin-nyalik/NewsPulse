87756a775f8ae1c19e6ce9ba381b713db05c35d0
MARVIN <o.silverygold@gmail.com>
2023-08-09 18:57:25 +0300

    Update app with dynamic content from API

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,72 +1,42 @@
+/* eslint-disable no-unused-vars */
 import css from '../assets/css/article.module.css';
-import avatar from '../assets/images/camera.jpg';
+import PropTypes from 'prop-types';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-const Article = () => {
- return (
-    <>
-    <Link to='/news/detail/name' className={css.link}>
-    <div className={css.container}>
-        <div className={css.info}>
-            <span className={css.tag}>Productivity</span>
-            <p className={css.title}>Title of Obama the Great</p>
-            <p className={css.footer}> <span><i className='bx bxs-news' /></span> Author <span className={css.footdate}>22/12/23</span></p>
-        </div>
-        <div className={css.image}>
-            <img src={avatar}/>
-        </div>
-    </div>
-    </Link>
-    <Link to='/news/detail/name1' className={css.link}>
-    <div className={css.container}>
-        <div className={css.info}>
-            <span className={css.tag}>Productivity</span>
-            <p className={css.title}>Title of Obama the Great</p>
-            <p className={css.footer}> <span><i className='bx bxs-news' /></span> Author <span className={css.footdate}>22/12/23</span></p>
-        </div>
-        <div className={css.image}>
-            <img src={avatar}/>
-        </div>
-    </div>
-    </Link>
-    <Link to='/news/detail/name2' className={css.link}>
-    <div className={css.container}>
-        <div className={css.info}>
-            <span className={css.tag}>Productivity</span>
-            <p className={css.title}>Title of Obama the Great</p>
-            <p className={css.footer}> <span><i className='bx bxs-news' /></span> Author <span className={css.footdate}>22/12/23</span></p>
-        </div>
-        <div className={css.image}>
-            <img src={avatar}/>
-        </div>
-    </div>
-    </Link>
-    <Link to='/news/detail/name3' className={css.link}>
-    <div className={css.container}>
-        <div className={css.info}>
-            <span className={css.tag}>Productivity</span>
-            <p className={css.title}>Title of Obama the Great</p>
-            <p className={css.footer}> <span><i className='bx bxs-news' /></span> Author <span className={css.footdate}>22/12/23</span></p>
-        </div>
-        <div className={css.image}>
-            <img src={avatar}/>
-        </div>
-    </div>
-    </Link>
-    <Link to='/news/detail/name4' className={css.link}>
-    <div className={css.container}>
-        <div className={css.info}>
-            <span className={css.tag}>Productivity</span>
-            <p className={css.title}>Title of Obama the Great</p>
-            <p className={css.footer}> <span><i className='bx bxs-news' /></span> Author <span className={css.footdate}>22/12/23</span></p>
-        </div>
-        <div className={css.image}>
-            <img src={avatar}/>
-        </div>
-    </div>
-    </Link>
-    </>
- )
+const Article = React.memo(({ articles }) => {
+  const secondArticles = useSelector((state) => state.articles.articles);
+  console.log('Articles from article component ', secondArticles);
+
+  return (
+    secondArticles.map(article => {
+      const encodedTitle = encodeURIComponent(article.url);
+      return (
+        <Link to={`/news/detail/${encodedTitle}`} className={css.link} key={article.title}>
+          <div className={css.container}>
+            <div className={css.info}>
+              <span className={css.tag}>{article.source.name}</span>
+              <p className={css.title}>{article.title}</p>
+              <p className={css.footer}>
+                <span><i className='bx bxs-news' /></span> {article.author}
+                <span className={css.footdate}>{article.publishedAt.substring(0,10)}</span>
+              </p>
+            </div>
+            <div className={css.image}>
+              <img src={article.urlToImage} alt='avatar' />
+            </div>
+          </div>
+        </Link>
+      );
+    })
+  );
+});
+
+Article.displayName = 'Article';
+
+Article.propTypes = {
+  articles: PropTypes.array.isRequired,
 }
 
 export default Article;
