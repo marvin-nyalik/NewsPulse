b2b8926682beca728ab632b92126a3e286f15fb0
MARVIN <o.silverygold@gmail.com>
2024-01-08 22:38:22 +0300

    Add RouteWrapper component

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,19 @@ import { ErrorFallback } from './main.jsx';
 function App() {
   const NewsDetail = React.lazy(() => import('./components/NewsDetail.jsx'));
 
+  const RouteWrapper = ({ children }) => (
+  <ErrorBoundary FallbackComponent={ErrorFallback} onReset={() => { console.log('Error'); }}>
+     {children}
+  </ErrorBoundary>
+  )
+
   return (
      <Router>
        <Routes>
-        <Route path='/' element={<Home />} />
-        <Route
-          path="/failing"
-          element={
-            <ErrorBoundary FallbackComponent={ErrorFallback} onReset={() => { console.log('Error'); }}>
-              <Failing />
-            </ErrorBoundary>
-          }
-        />
-        <Route path='/news/detail/:title'
-          element={<Suspense fallback ={<div> Loading ... </div>}> <NewsDetail /> </Suspense> } />
+          <Route path='/' element={<RouteWrapper> <Home /></RouteWrapper>} />
+          <Route path="/failing" element={<RouteWrapper> <Failing /></RouteWrapper>} />
+          <Route path='/news/detail/:title'
+            element={<Suspense fallback ={<div> Loading ... </div>}> <NewsDetail /> </Suspense> } />
        </Routes> 
      </Router>
   )
